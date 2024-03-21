0f71aaf7467196fa10a6fcd454f9e2fb612b1243
priyamvashishtha1 <priyamvashishtha60@gmail.com>
2024-03-21 23:22:45 +0530

    remove unwanted items

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { About } from "./components/About";
-import { Cta } from "./components/Cta";
-import { FAQ } from "./components/FAQ";
+//import { Cta } from "./components/Cta";
+//import { FAQ } from "./components/FAQ";
 import { Features } from "./components/Feature";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
-import { HowItWorks } from "./components/HowItWorks";
+//import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
-import { Newsletter } from "./components/Newsletter";
-import { Pricing } from "./components/Pricing";
+//import { Newsletter } from "./components/Newsletter";
+//import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
 import { Services } from "./components/Services";
-import { Sponsors } from "./components/Sponsors";
-import { Team } from "./components/Team";
-import { Testimonials } from "./components/Testimonials";
+//import { Sponsors } from "./components/Sponsors";
+//import { Team } from "./components/Team";
+//import { Testimonials } from "./components/Testimonials";
 import "./App.css";
 
 function App() {
@@ -22,18 +22,9 @@ function App() {
       <Hero />
       <Features />
       <About />
-      <HowItWorks />
-      
-      <Services />
-      <Cta />
-      <Testimonials />
-      <Team />
-      <Pricing />
-      <Newsletter />
-      <FAQ />
       <Footer />
       <ScrollToTop />
-      <Sponsors />
+    
     </>
   );
 }
